3af5bc0146c56612c241e2722383bfa90d4476da
amireddym <amireddy.m@northeastern.edu>
2021-09-20 13:35:28 -0400

    Added initial model and Ui level code

---
+++ Assignment-1/Lab_1/src/model/Person.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author manojreddy
+ */
+public class Person {
+    
+    private String name;
+    private String geographicData;
+    private String dateOfBirth;
+    private int telephoneNo;
+    private int faxNo;
+    private String emailAddress;
+    private String ssn;
+    private String medicalRecordNo;
+    private String healthPlanBeneficiaryNo;
+    private long bankAccountNo;
+    private String licenseNo;
+    private String vehicleIdentifiers;
+    private String deviceIdentifiers;
+    private String linkedIn;
+    private String ipAddress;
+    private String biometricIdentifiers;
+    private String photos;
+    private String uniqueIdentity;
+    
+    
+    
+}
