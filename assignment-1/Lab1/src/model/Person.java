19cf548f1b534d521d638da06e1b03535512a652
amireddym <amireddy.m@northeastern.edu>
2021-09-25 17:01:12 -0400

    Initial commit with basic validations

---
+++ assignment-1/Lab1/src/model/Person.java
@@ -0,0 +1,171 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.List;
+import java.util.Date;
+
+/**
+ *
+ * @author manojreddy
+ */
+public class Person {
+    
+    private String name;
+    private String geographicData;
+    private Date dob;
+    private long telephoneNo;
+    private long faxNo;
+    private String emailId;
+    private long ssn;
+    private long medicalRecordNo;
+    private long healthPlanBenficiaryNo;
+    private long bankAccountNo;
+    private long licenseNo;
+    private String vehicleIdentifier;
+    private String deviceIdentifier;
+    private String linkedIn;
+    private String ipAddress;
+    private String biometric;
+    private String uniqueNo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographicData() {
+        return geographicData;
+    }
+
+    public void setGeographicData(String geographicData) {
+        this.geographicData = geographicData;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public long getTelephoneNo() {
+        return telephoneNo;
+    }
+
+    public void setTelephoneNo(long telephoneNo) {
+        this.telephoneNo = telephoneNo;
+    }
+
+    public long getFaxNo() {
+        return faxNo;
+    }
+
+    public void setFaxNo(long faxNo) {
+        this.faxNo = faxNo;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(long ssn) {
+        this.ssn = ssn;
+    }
+
+    public long getMedicalRecordNo() {
+        return medicalRecordNo;
+    }
+
+    public void setMedicalRecordNo(long medicalRecordNo) {
+        this.medicalRecordNo = medicalRecordNo;
+    }
+
+    public long getHealthPlanBenficiaryNo() {
+        return healthPlanBenficiaryNo;
+    }
+
+    public void setHealthPlanBenficiaryNo(long healthPlanBenficiaryNo) {
+        this.healthPlanBenficiaryNo = healthPlanBenficiaryNo;
+    }
+
+    public long getBankAccountNo() {
+        return bankAccountNo;
+    }
+
+    public void setBankAccountNo(long bankAccountNo) {
+        this.bankAccountNo = bankAccountNo;
+    }
+
+    public long getLicenseNo() {
+        return licenseNo;
+    }
+
+    public void setLicenseNo(long licenseNo) {
+        this.licenseNo = licenseNo;
+    }
+
+    public String getVehicleIdentifier() {
+        return vehicleIdentifier;
+    }
+
+    public void setVehicleIdentifier(String vehicleIdentifier) {
+        this.vehicleIdentifier = vehicleIdentifier;
+    }
+
+    public String getDeviceIdentifier() {
+        return deviceIdentifier;
+    }
+
+    public void setDeviceIdentifier(String deviceIdentifier) {
+        this.deviceIdentifier = deviceIdentifier;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getBiometric() {
+        return biometric;
+    }
+
+    public void setBiometric(String biometric) {
+        this.biometric = biometric;
+    }
+
+    public String getUniqueNo() {
+        return uniqueNo;
+    }
+
+    public void setUniqueNo(String uniqueNo) {
+        this.uniqueNo = uniqueNo;
+    }
+
+}
